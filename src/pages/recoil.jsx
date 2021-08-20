3936bda23d45e714f5dfa84f988b6bc4e8ec4b21
liupan <liupan@nti56.com>
2021-08-20 16:44:57 +0800

    1

--- src/pages/recoil.jsx
+++ src/pages/recoil.jsx
@@ -1,3 +1,8 @@
- const Recoil = () => <a href='https://www.recoiljs.cn/' target='_blank' rel='noreferrer'>Recoil</a>
+const Recoil = () => (
+  <h2>
+    基于 Hook 的状态管理库：
+    <a href='https://www.recoiljs.cn/' target='_blank' rel='noreferrer'>Recoil</a>
+  </h2>
+)
 
- export default Recoil
+export default Recoil
